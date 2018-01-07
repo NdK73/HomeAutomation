adb214ed8df61c6cada5f7c5f7cf5065ff0cb5a8
NdK <ndk@csshl.org>
2018-01-07 16:21:17 +0100

    Changed dom-send.py to be able to send multicast packets

--- protocol/dom-send.py
+++ protocol/dom-send.py
@@ -7,8 +7,14 @@ import sys
 import socket
 
 #ANY = "0.0.0.0" 
-#MCAST_ADDR = "239.255.215.74"
+MCAST_ADDR = "239.255.215.74"
 MCAST_PORT = 55114
+receive = True
+
+server = sys.argv[1];
+if ( "*" == server ) :
+    server=MCAST_ADDR
+    receive = False
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
@@ -16,19 +22,13 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 # Allow multiple sockets to use the same PORT number
 sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 
-# Bind to the port that we know will receive multicast data
-#sock.bind((ANY,MCAST_PORT))
-
-# Tell the kernel that we want to add ourselves to a multicast group
-# The address for the multicast group is the third param
-#status = sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, socket.inet_aton(MCAST_ADDR) + socket.inet_aton(ANY))
-#status = sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, socket.inet_aton(MCAST_ADDR))
-
 # setblocking(0) is equiv to settimeout(0.0) which means we poll the socket.
 # But this will raise an error if recv() or send() can't immediately find or send data. 
 sock.setblocking(1)
 
 #sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 #sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
-sock.sendto(bytes(sys.argv[2], "utf-8"), (sys.argv[1], MCAST_PORT))
-print(sock.recv(1500).decode("utf-8"), end='', flush=True)
+sock.sendto(bytes(sys.argv[2], "utf-8"), (server, MCAST_PORT))
+
+if ( receive ) :
+    print(sock.recv(1500).decode("utf-8"), end='', flush=True)
