6d22f0cd085ba9e93e393ea1b027e1ede301c9e3
NdK <ndk@csshl.org>
2018-05-24 23:29:20 +0200

    Added error decoding to dom-send.py

--- protocol/dom-send.py
+++ protocol/dom-send.py
@@ -11,6 +11,19 @@ MCAST_ADDR = "239.255.215.74"
 MCAST_PORT = 55114
 receive = True
 
+# This maps error codes to human-readable strings (see Domotic.h)
+ErrorCodes={
+  "01": "ERR_CTX",
+  "80": "CMD_BAD",
+  "81": "CMD_UNS",
+  "82": "CMD_RANGE",
+  "83": "CMD_SIZE",
+  "90": "INF_BAD",
+  "91": "INF_RANGE",
+  "FE": "UNSUPP",
+  "FF": "UNKNOWN"
+}
+
 server = sys.argv[1];
 if ( "*" == server ) :
     server=MCAST_ADDR
@@ -29,7 +42,17 @@ sock.setblocking(1)
 #sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 #sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
 sock.sendto(bytes(sys.argv[2], "utf-8"), (server, MCAST_PORT))
+sock.settimeout(1.0)
 
 if ( receive ) :
     #print(sock.recv(1500).decode("utf-8"), end='', flush=True)
-    print(sock.recv(1500).decode("utf-8"), flush=True)
+    ans=sock.recv(1500).decode("utf-8")
+    print(ans, flush=True)
+    if(ans[:1]!="A") :
+        sys.exit("Invalid packet")
+    else :
+        print("Answer packet")
+    if(ans[1:3]=="00") :
+        print(" Ok (No error)")
+    else :
+        sys.exit(ErrorCodes[ans[1:3]]);
